f3709b8337673b9791e2e9b2910f874b00e4e96b
echoua <rkulchytskyi@echoua.com>
2018-06-26 18:01:39 +0300

    Prepared for deploy 3

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -22,7 +22,6 @@ mongoose
     .connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
-
 
 // Passport middleware
 app.use(passport.initialize());
@@ -35,17 +34,15 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// THE START SETUP DEPLOY WITH HEROKU
-
 // Server static assets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+    });
 }
-// END START SETUP DEPLOY WITH HEROKU
 
 const port = process.env.PORT || 5000;
 
